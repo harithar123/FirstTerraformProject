ed1a5572eca66998d74d7c98cb556e018e19653d
Ubuntu <ubuntu@ip-172-31-93-251.ec2.internal>
2024-08-25 00:24:47 +0000

    comment

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "TerraformJenkins" {
+   ami = "ami-0e86e20dae9224db8"
+   instance_type = "t2.micro"
+   tags = {
+     Name = "TF-Instance"
+   }
+}
