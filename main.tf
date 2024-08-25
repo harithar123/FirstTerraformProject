1746d481f0b020274b8bae292e7a8cc694076b5d
Ubuntu <ubuntu@ip-172-31-93-251.ec2.internal>
2024-08-25 00:58:20 +0000

    first commit

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "TerraformJenkins" {
    ami = "ami-0e86e20dae9224db8"
    instance_type = "t2.micro"
    tags = {
-     Name = "TF-Instance"
+     Name = "TF-Instance1"
    }
 }
